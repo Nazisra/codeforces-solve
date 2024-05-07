7dc08041818d4ca95f867348278d3d25b2f049f5
Ahmed Nazmul Islam Rahad <128446695+Nazisra@users.noreply.github.com>
2024-05-07 20:56:09 +0600

    Create Check if it is possible to survive on Island.cpp

---
+++ Check if it is possible to survive on Island.cpp
@@ -0,0 +1,58 @@
+
+//link
+//https://www.geeksforgeeks.org/problems/check-if-it-is-possible-to-survive-on-island4922/1
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution{
+public:
+    int minimumDays(int S, int N, int M){
+        int sunday=S/7;
+        int buyingdays=S-sunday;
+        
+        int totalfood=S*M;
+        int ans=0;
+        
+        if(totalfood%N) return ans= totalfood/N;
+        else{
+            ans=totalfood/N;
+            ans+=1;
+        }
+        
+        if(ans<=buyingdays)
+        return ans;
+        
+        else{
+            return -1;
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+};
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int S, N, M;
+        cin>> S >> N >> M;
+        
+        Solution ob;
+        cout<<ob.minimumDays(S, N, M)<<endl;
+    }
+    return 0;
+}
